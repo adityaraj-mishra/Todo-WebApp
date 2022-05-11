1a3bfe66040ffdcd6dac0be16b85768ab414091e
adityaraj-mishra <adityaraj-mishra@users.noreply.github.com>
2022-05-11 23:05:07 +0530

    changed settings

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,4 +126,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #Activate Django Heroku
-django_heroku.settings(locals())
